6f6b0450604671b99f376867e5d440ed9591493a
Unknown <mdiesen@gmail.com>
2018-06-11 16:55:10 +0200

    Added Access-Control-Allow-Methods

--- middleware/headers.go
+++ middleware/headers.go
@@ -22,6 +22,7 @@ func ContentTypeHandler(next http.Handler) http.HandlerFunc {
 func StandardHeadersHandler(next http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 		w.Header().Set("Content-Type", fmt.Sprintf("%s; charset=utf-8", "application/json"))
 		next.ServeHTTP(w, r)
 	}
