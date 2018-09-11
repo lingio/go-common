81e1273ee15df38971d33ee3406af5225abbeca8
Huy Huỳnh <huy@lingio.co>
2018-09-11 13:45:16 +0700

    add function check permission

--- auth/firebase.go
+++ auth/firebase.go
@@ -5,6 +5,8 @@ import (
 	"firebase.google.com/go"
 	"context"
 	"google.golang.org/api/option"
+	"reflect"
+	"errors"
 )
 
 type FireBase struct {
@@ -30,4 +32,21 @@ func (f *FireBase) VerifyToken(ctx context.Context, idToken string) (*auth.Token
 		return nil, err
 	}
 	return token, nil
+}
+
+func (f *FireBase) AuthorizeForPrincipal(condition map[string]string, principal interface{}) (bool, error) {
+	v := reflect.ValueOf(principal).Elem()
+	if v.Kind() != reflect.Struct {
+		return false, errors.New("principal must a struct")
+	}
+	for k, cv := range condition {
+		pv := v.FieldByName(k)
+		if !pv.IsValid() {
+			return false, errors.New("principal don't have key name: " + k)
+		}
+		if pv.String() != cv {
+			return false, nil
+		}
+	}
+	return true, nil
 }
