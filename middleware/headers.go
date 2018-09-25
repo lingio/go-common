fb5c3d8ac22f8516a55b1f497ff64b3786e4fd4d
Huy Huỳnh <huy@lingio.co>
2018-09-25 18:23:55 +0700

    return domain in Allowed Origin when request header has

--- middleware/headers.go
+++ middleware/headers.go
@@ -21,7 +21,11 @@ func ContentTypeHandler(next http.Handler) http.HandlerFunc {
 
 func StandardHeadersHandler(next http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		origin := r.Header.Get("Origin")
+		if origin == "" {
+			origin = "*"
+		}
+		w.Header().Set("Access-Control-Allow-Origin", origin)
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		w.Header().Set("Content-Type", fmt.Sprintf("%s; charset=utf-8", "application/json"))
