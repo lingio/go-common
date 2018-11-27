4c9eba97ad997cd1e9f734e7e156204cc097010f
Unknown <mdiesen@gmail.com>
2018-11-27 21:47:50 +0100

    Added a helper function to check for nil

---
+++ checks/check.go
@@ -0,0 +1,13 @@
+package checks
+
+import "errors"
+
+func CheckNil(params... interface{}) error {
+	for i, p := range params {
+		if p == nil {
+			return errors.New("Unexpected null pointer at param " + string(i))
+		}
+	}
+	return nil
+}
+
