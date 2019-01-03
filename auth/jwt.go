832ee4706b354f8cc2c3990e3fb4d847b3ac4362
Huy Huynh <huyhvq@icloud.com>
2019-01-03 09:48:20 +0700

    rename struct

--- auth/jwt.go
+++ auth/jwt.go
@@ -7,7 +7,7 @@ import (
 	"github.com/dgrijalva/jwt-go"
 )
 
-type JwtTokenVerifier interface {
+type TokenVerifier interface {
 	VerifyToken(token string) (map[string]interface{}, error)
 	GetPrincipalWithToken(tokenString string, principal AuthenticatePrincipal) (AuthenticatePrincipal, error)
 }
@@ -16,14 +16,14 @@ type AuthenticatePrincipal interface {
 	MapData(map[string]interface{}) error
 }
 
-type JwtTokenVerify struct {
+type RsaTokenVerifier struct {
 	verifyKey *rsa.PublicKey
 }
 
-var _ JwtTokenVerifier = &JwtTokenVerify{}
+var _ TokenVerifier = &RsaTokenVerifier{}
 
-// Create new struct JwtTokenVerifier
-// keyFunc will receive the public key and return struct as JwtTokenVerifier.
+// Create new struct TokenVerifier
+// keyFunc will receive the public key and return struct as TokenVerifier.
 // Example:
 // key, err := ioutil.ReadFile("lingio-rsa256")
 // if err != nil {
@@ -33,22 +33,22 @@ var _ JwtTokenVerifier = &JwtTokenVerify{}
 // if err != nil {
 // 		os.Exit(1)
 // }
-// verifier := NewJwtTokenVerify(publicKey)
-func NewJwtTokenVerify(verifyKey *rsa.PublicKey) JwtTokenVerifier {
-	return &JwtTokenVerify{verifyKey: verifyKey}
+// verifier := NewTokenVerify(publicKey)
+func NewTokenVerify(verifyKey *rsa.PublicKey) TokenVerifier {
+	return &RsaTokenVerifier{verifyKey: verifyKey}
 }
 
 // Parse, validate  a token.
 // keyFunc will receive the token string and return claims data as map[string]interface{}.
 // If everything is okay, err will be nil
 // Example:
-// verifier := NewJwtTokenVerify(publicKey)
+// verifier := NewTokenVerify(publicKey)
 // data, err := verifier.VerifyToken("tokenString")
 // if err != nil {
 // 		fmt.Errorf("failed with err: %v",err)
 // }
 // partnerID := data["partnerId"]
-func (j *JwtTokenVerify) VerifyToken(tokenString string) (map[string]interface{}, error) {
+func (j *RsaTokenVerifier) VerifyToken(tokenString string) (map[string]interface{}, error) {
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
@@ -71,14 +71,14 @@ func (j *JwtTokenVerify) VerifyToken(tokenString string) (map[string]interface{}
 // keyFunc will receive the token string, principal interface and return mapped principal data as AuthenticatePrincipal.
 // If everything is okay, err will be nil
 // Example:
-// verifier := NewJwtTokenVerify(publicKey)
+// verifier := NewTokenVerify(publicKey)
 // principal := models.AuthenticatePrincipal{}
 // mappedPrincipal, err := verifier.GetPrincipalWithToken("tokenString",principal)
 // if err != nil {
 // 		fmt.Errorf("failed with err: %v",err)
 // }
 // partnerID := mappedPrincipal.PartnerID
-func (j *JwtTokenVerify) GetPrincipalWithToken(tokenString string, principal AuthenticatePrincipal) (AuthenticatePrincipal, error) {
+func (j *RsaTokenVerifier) GetPrincipalWithToken(tokenString string, principal AuthenticatePrincipal) (AuthenticatePrincipal, error) {
 	claims, err := j.VerifyToken(tokenString)
 	if err != nil {
 		return nil, err
