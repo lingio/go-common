14b8cc8657f8ce9d4f70864c172a2ca4eb024cf9
Unknown <mdiesen@gmail.com>
2019-12-30 16:30:49 +0100

    Added message at end of a request

--- middleware/request_logger.go
+++ middleware/request_logger.go
@@ -11,9 +11,9 @@ type RequestLogger struct {
 	ll *log.LingioLogger
 }
 
-// FIXME: Apparently go ResponseWriters use a lot of "hidden" (implemented but not explicitly so)interfaces to be usable
-// this creates the problem that the wrapped response writer could hide some functionaliy from the later stages of the request hander
-// This would result in loss of functionality e.g streaming would not work becase the flushing interface is not implemented
+// Apparently go ResponseWriters use a lot of "hidden" (implemented but not explicitly so)interfaces to be usable
+// this creates the problem that the wrapped response writer could hide some functionality from the later stages of the request handler
+// This would result in loss of functionality e.g streaming would not work because the flushing interface is not implemented
 type requestLoggerResponseWriter struct {
 	writer     http.ResponseWriter
 	statuscode int
@@ -56,7 +56,6 @@ func (t *RequestLogger) RequestLogHandler(next http.Handler) http.Handler {
 		w2 := newRequestLoggerResponseWriterResponseWriter(w)
 		next.ServeHTTP(w2, r)
 
-		// FIXME: We want some kind of message here!
-		t.ll.LogEndOfHTTPRequest(ctx, "", w2.statuscode, r, time.Since(startTime))
+		t.ll.LogEndOfHTTPRequest(ctx, "request completed", w2.statuscode, r, time.Since(startTime))
 	})
 }
