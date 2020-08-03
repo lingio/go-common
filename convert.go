e6d9eccdc3c04897de2630b66cd916af1bb6b08c
morten <morten@lingio.com>
2020-08-03 10:46:25 +0200

    Added conversions

---
+++ convert.go
@@ -0,0 +1,67 @@
+package common
+
+import "time"
+import openapi_types "github.com/deepmap/oapi-codegen/pkg/types"
+
+func Bool(b *bool) bool {
+	if b != nil && *b {
+		return true
+	}
+	return false
+}
+
+func Int(i *int) int {
+	if i == nil {
+		return 0
+	}
+	return *i
+}
+
+func Str(s *string) string {
+	if s == nil {
+		return ""
+	}
+	return *s
+}
+
+func Time(t *time.Time) time.Time {
+	if t == nil {
+		return time.Time{}
+	}
+	return *t
+}
+
+func Date(d *openapi_types.Date) openapi_types.Date {
+	if d == nil {
+		return openapi_types.Date{}
+	}
+	return *d
+}
+
+func StrP(s string) *string {
+	if s == "" {
+		return nil
+	}
+	return &s
+}
+
+func TimeP(t time.Time) *time.Time {
+	if t.IsZero() {
+		return nil
+	}
+	return &t
+}
+
+func DateP(d openapi_types.Date) *openapi_types.Date {
+	if d.IsZero() {
+		return nil
+	}
+	return &d
+}
+
+func DatePFromTime(t time.Time) *openapi_types.Date {
+	if t.IsZero() {
+		return nil
+	}
+	return &openapi_types.Date{Time: t}
+}
