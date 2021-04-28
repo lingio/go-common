259a8d0f586650d60dd4b8babbaabc3181cf99c6
Johan Larsson <jl311888@gmail.com>
2021-04-28 10:10:04 +0200

    add template variable for object ID

--- storagegen/main.go
+++ storagegen/main.go
@@ -17,6 +17,7 @@ type BucketSpec struct {
 	DbTypeName string
 	BucketName string
 	Template   string
+	IdName     *string
 }
 
 type StorageSpec struct {
@@ -47,11 +48,16 @@ func main() {
 	dir := path.Dir(os.Args[1])
 
 	for _, b := range spec.Buckets {
+		in := "ID"
+		if b.IdName != nil {
+			in = *b.IdName
+		}
 		bytes := generate("tmpl/"+b.Template, TmplParams{
 			ServiceName: spec.ServiceName,
 			TypeName:    b.TypeName,
 			DbTypeName:  b.DbTypeName,
 			BucketName:  b.BucketName,
+			IdName:      in,
 		})
 		err := ioutil.WriteFile(fmt.Sprintf("%s/%s.gen.go", dir, b.BucketName), bytes, 0644)
 		if err != nil {
@@ -107,6 +113,7 @@ type TmplParams struct {
 	DbTypeName  string
 	BucketName  string
 	ServiceName string
+	IdName      string
 }
 
 type TmplParams2 struct {
@@ -116,7 +123,7 @@ type TmplParams2 struct {
 func generate(tmplFilename string, params interface{}) []byte {
 	tpl, err := template.ParseFiles(tmplFilename)
 	if err != nil {
-		zl.Fatal().Str("tmplFilename", tmplFilename).Str("err", err.Error()).Msg("failed to load message template")
+		zl.Fatal().Str("tmplFilename", tmplFilename).Str("err", err.Error()).Msg("failed to load template")
 	} else if tpl == nil {
 		zl.Fatal().Str("tmplFilename", tmplFilename).Msg("template is nil. failed to load message template")
 	}
