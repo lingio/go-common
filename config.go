aee8c6a29fbdebeab84d0aa960f7a513bc1b41fb
Axel <axel@lingio.com>
2022-03-10 09:50:32 +0100

    config: add minio and redis config

---
+++ config.go
@@ -0,0 +1,16 @@
+package common
+
+// MinioConfig describes the required fields to setup a s3 minio client.
+type MinioConfig struct {
+	Host        string
+	AccessKeyID string
+	SSL         bool `json:"SSL"`
+}
+
+// RedisConfig describes connectivity options for setting up a redis client
+// using the included SetupRedisClient func in this pkg.
+type RedisConfig struct {
+	Addr       string // for testing locally using one redis server
+	MasterName string // sentinel master
+	ServiceDNS string // lookup sentinel servers on this domain name
+}
