a88e5c6ea47ed0cb2335ee064b5dd6913f5ff987
Axel <axel@lingio.com>
2022-03-24 11:33:19 +0100

    encrypted store: implement crypto scheme v2

--- encrypted_store.go
+++ encrypted_store.go
@@ -1,41 +1,51 @@
 package common
 
 import (
+	"bytes"
 	"context"
 	"crypto/aes"
 	"crypto/cipher"
+	"crypto/rand"
 	"encoding/base32"
 	"errors"
 	"fmt"
+	"io"
 	"net/http"
 )
 
-var ErrDecrypt = errors.New("encrypted_store: decryption error")
+var ErrDecrypt = errors.New("encrypted store: decryption error")
+
+var encstoreV1Header = []byte{}               // there was no header in version 1 :(
+var encstoreV2Header = [...]byte{'v', 2, '/'} // magic byte sequence for detecting encryption store scheme
 
 // EncryptedStore
 type EncryptedStore struct {
 	backend LingioStore
-	// AES block cipher is safe for concurrent use
-	cipher cipher.Block
+
+	cipher cipher.Block // v1: ciphertext[16b]||plaintext
+	aesgcm cipher.AEAD  // v2: header[3b]||nonce[12b]||ciphertext
 }
 
 // NewEncryptedStore
 func NewEncryptedStore(backend LingioStore, cipherKey string) (*EncryptedStore, error) {
-	if cipherKey == "" {
-		return nil, errors.New("cipherKey must not be empty")
-	}
 	if len(cipherKey) != 32 {
 		return nil, errors.New("cipherKey must be 32 chars")
 	}
 
-	cipher, err := aes.NewCipher([]byte(cipherKey))
+	block, err := aes.NewCipher([]byte(cipherKey))
 	if err != nil {
 		return nil, fmt.Errorf("cipher: %w", err)
 	}
 
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, fmt.Errorf("aes gcm: %w", err)
+	}
+
 	return &EncryptedStore{
 		backend: backend,
-		cipher:  cipher,
+		cipher:  block,
+		aesgcm:  aesgcm,
 	}, nil
 }
 
@@ -44,18 +54,22 @@ func (es *EncryptedStore) GetObject(file string) ([]byte, ObjectInfo, *Error) {
 	if err != nil {
 		return nil, ObjectInfo{}, err
 	}
-	es.cipher.Decrypt(data, data)
+
 	if filename, err := es.decryptFilename(info.Key); err != nil {
 		return nil, ObjectInfo{}, NewErrorE(http.StatusInternalServerError, err)
 	} else {
 		info.Key = filename
 	}
-	return data, info, nil
+	plaintext := es.decrypt(data)
+
+	return plaintext, info, nil
 }
 
 func (es *EncryptedStore) PutObject(ctx context.Context, file string, data []byte) (ObjectInfo, *Error) {
-	es.cipher.Encrypt(data, data)
-	info, err := es.backend.PutObject(ctx, es.encryptFilename(file), data)
+	encdata := es.encrypt(data)
+	encfile := es.encryptFilename(file)
+
+	info, err := es.backend.PutObject(ctx, encfile, encdata)
 	if err != nil {
 		return ObjectInfo{}, err
 	}
@@ -93,11 +107,7 @@ func (es EncryptedStore) StoreName() string {
 }
 
 func (es *EncryptedStore) encryptFilename(file string) string {
-	tmp := []byte(file)
-	if len(tmp) < 16 {
-		panic(fmt.Errorf("%s: file length < 16 bytes", file))
-	}
-	es.cipher.Encrypt(tmp, tmp)
+	tmp := es.encrypt([]byte(file))
 	return base32.StdEncoding.EncodeToString(tmp)
 }
 
@@ -105,13 +115,50 @@ func (es *EncryptedStore) decryptFilename(file string) (_ string, err error) {
 	// guard against Decrypt throwing a panic
 	defer func() {
 		if r := recover(); r != nil {
-			err = ErrDecrypt
+			err = fmt.Errorf("%w: %v", ErrDecrypt, r)
 		}
 	}()
 	tmp, err := base32.StdEncoding.DecodeString(file)
 	if err != nil {
-		return "", fmt.Errorf("%s: %w", file, err)
+		return "", fmt.Errorf("base32 decode: %q: %w", file, err)
 	}
-	es.cipher.Decrypt(tmp, tmp)
+	tmp = es.decrypt(tmp)
 	return string(tmp), nil
+}
+
+func (es *EncryptedStore) decrypt(data []byte) []byte {
+	var plaintext []byte
+	if bytes.HasPrefix(data, encstoreV2Header[:]) {
+		offset := len(encstoreV2Header)
+		nonce := data[offset : offset+es.aesgcm.NonceSize()]
+		offset += es.aesgcm.NonceSize()
+		ciphertext := data[offset:]
+
+		var err error
+		plaintext, err = es.aesgcm.Open(nil, nonce, ciphertext, nil)
+		if err != nil {
+			panic(fmt.Errorf("encrypted store: decrypt: %w", err))
+		}
+	} else {
+		// fallback to v1 scheme which only encrypted the first block (16 bytes)
+		es.cipher.Decrypt(data, data)
+		plaintext = data
+	}
+	return plaintext
+}
+
+func (es *EncryptedStore) encrypt(data []byte) []byte {
+	nonce := make([]byte, es.aesgcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(fmt.Errorf("encrypted store: could not generate nonce: %w", err))
+	}
+
+	// ciphertext reuses data slice
+	ciphertext := es.aesgcm.Seal(data[:0], nonce, data, nil)
+
+	// header||nonce||ciphertext
+	blob := encstoreV2Header[:]
+	blob = append(blob, nonce...)
+	blob = append(blob, ciphertext...)
+	return blob
 }
