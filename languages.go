968fc9bd47f66c1e30ec40e82b1746c87cb2db1a
Axel <axel@lingio.com>
2023-05-30 13:26:08 +0200

    add language mappings

---
+++ languages.go
@@ -0,0 +1,90 @@
+package common
+
+import "net/http"
+
+func LangCodeName(langCode string) (column string, err error) {
+	switch langCode {
+	case "sv":
+		column = "Swedish"
+	case "en":
+		column = "English"
+	case "ar":
+		column = "Arabic"
+	case "ckb":
+		column = "Sorani"
+	case "es":
+		column = "Spanish"
+	case "fa":
+		column = "Farsi"
+	case "fa-AF":
+		column = "Dari"
+	case "fr":
+		column = "French"
+	case "pl":
+		column = "Polish"
+	case "ru":
+		column = "Russian"
+	case "so":
+		column = "Somali"
+	case "th":
+		column = "Thai"
+	case "ti":
+		column = "Tigrinya"
+	case "tr":
+		column = "Turkish"
+	case "uk":
+		column = "Ukrainian"
+	case "ro":
+		column = "Romanian"
+	case "bs":
+		column = "Bosnian"
+	case "vi":
+		column = "Vietnamese"
+	default:
+		err = NewError(http.StatusBadRequest).Msg("langCode not valid").Str("langCode", langCode)
+	}
+	return
+}
+
+func LangCode(lang string) string {
+	switch lang {
+	case "Swedish":
+		return "sv"
+	case "English":
+		return "en"
+	case "Arabic":
+		return "ar"
+	case "Spanish":
+		return "es"
+	case "Farsi":
+		return "fa"
+	case "Dari":
+		return "fa-AF"
+	case "French":
+		return "fr"
+	case "Polish":
+		return "pl"
+	case "Russian":
+		return "ru"
+	case "Somali":
+		return "so"
+	case "Sorani":
+		return "ckb"
+	case "Thai":
+		return "th"
+	case "Tigrinya":
+		return "ti"
+	case "Turkish":
+		return "tr"
+	case "Ukrainian":
+		return "uk"
+	case "Romanian":
+		return "ro"
+	case "Bosnian":
+		return "bs"
+	case "Vietnamese":
+		return "vi"
+	default:
+		panic("lang not valid: " + lang)
+	}
+}
